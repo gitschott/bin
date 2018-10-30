5aa3504f3b7e64ea9e5665a1c7707d457b336578
Jason Schott <jschott@vendini.com>
2018-10-30 02:23:55 -0700

    1

---
+++ create-staging1-vhosts.sh
@@ -0,0 +1,26 @@
+#setup ssh
+mkdir -p /home/jschott/.ssh
+chmod 700 /home/jschott/.ssh
+touch /home/jschott/.ssh/authorized_keys
+touch /home/jschott/.ssh/authorized_keys
+chmod 600 /home/jschott/.ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDCjD7cNLtoGhGVqzGGWEkclGS2aFn26PU7mp8GNpUb9thEpGLiNIThjON/vb3pWqziuxOkOtj1K+gxYD289YpjsBnu3GonPzv1wkPBgg4B7gclB+JD25jg/UU8sr+jV2h+jf2iJv2WobW+AMXFxnuC+fn+Fx+Qjf8gARFvRaJLe2UMYYRNRvP9sPDfEhus3vqfgjgLCXbYskJE5kIt8d+Ah+SSLWifI8l7z71obk6wpuW/fDFuMggK53NFQJaNr0wpgWbfVR1X2wc+gAXospc24q1V05EtktZ7Ilc+xVIUPFPx6t8bnrs2YCNFPbQv21PaglhGcbb/Hi3joQA/ZS+X jschott@jschott-mbp.local" >> /home/jschott/.ssh/authorized_keys
+#download scripts
+#install git
+apt-get install git
+wget 
+
+
+
+
+virtualhost create esstaging2.ticketmob.com D:\Sites\BrandPropertySites
+virtualhost create lsstaging2.ticketmob.com D:\Sites\BrandPropertySites
+virtualhost create tmstaging2.ticketmob.com D:\Repositories\TicketMob\src
+virtualhost create tsstaging2.ticketmob.com D:\Sites\BrandPropertySites
+virtualhost create ssstaging2.ticketmob.com D:\Sites\SelfServe
+virtualhost create staging2-admin.ticketmob.com D:\Sites\Ticketmob_admin
+virtualhost create staging2-embed.ticketmob.com D:\Sites\LaughStub.com
+virtualhost create bostaging2.ticketmob.com D:\ticketmobboxoffice.com
+virtualhost create staging2-secure.ticketmob.com D:\Sites\TemplateSites\responsive
+virtualhost create staging2-torchly.ticketmob.com d:\sites\torchly\
+virtualhost create staging2-venueadmin.crowdtorch.com D:\sites\ticketmob\
